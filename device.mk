6df5654fb1df371e26bfdca38eeb81c42842b2d7
BlackMesa123 <giangrecosalvo9@gmail.com>
2022-09-04 03:12:34 +0200

    a52sxq: introduce crypto support
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- device.mk
+++ device.mk
@@ -22,6 +22,17 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
+# qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH)
